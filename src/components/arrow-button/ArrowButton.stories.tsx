e64e2a48e99dd9d83f8fca202499ffeb37d2fc22
Cheremis <alseta88@yandex.ru>
2024-03-03 14:33:03 +0300

    Test commit

--- src/components/arrow-button/ArrowButton.stories.tsx
+++ src/components/arrow-button/ArrowButton.stories.tsx
@@ -13,7 +13,7 @@ export const ArrowButtonStory: Story = {
 	render: () => {
 		return (
 			<>
-				<ArrowButton onClick={() => console.log('ddd')} isOpen />
+				<ArrowButton onClick={() => console.log('Open-close')} isOpen />
 			</>
 		);
 	},
